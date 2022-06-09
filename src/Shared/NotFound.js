27e8efe4af8e0c373041221d4b640c2ec9489a24
Md-Mobassher <you@example.com>
2022-06-09 10:39:24 +0600

    first commit

---
+++ src/Shared/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <img className=' w-1/2 d-block mx-auto' src={"https://i.ibb.co/rss80m5/404.png"} alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
