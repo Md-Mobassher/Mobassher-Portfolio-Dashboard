a1daab5a354309ea15afa80713c29ccf83de9a04
Md-Mobassher <you@example.com>
2023-03-19 13:42:20 +0600

    service icon color change

--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -9,7 +9,7 @@ const Services = () => {
             <h2 className='lg:text-4xl md:text-3xl text-2xl font-bold text-primary text-center lg:mb-12 mb-8'>Services</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 justify-center items-center px-5'>
                 <div className=' max-w-sm mx-auto flex-col justify-center items-center shadow-2xl rounded-lg border-x-rose-600 p-5 '>
-                    <FaLaptopCode className='w-10 h-10 block mx-auto mt-4 mb-6 '></FaLaptopCode>
+                    <FaLaptopCode className='w-10 h-10 block mx-auto mt-4 mb-6 text-secondary'></FaLaptopCode>
                     <div >
                         <h2 className='text-primary font-semibold lg:text-2xl text-xl text-center mb-4'>Web Design</h2>
                         <div>
@@ -18,14 +18,14 @@ const Services = () => {
                     </div>
                 </div>
                 <div className=' max-w-sm mx-auto  flex-col justify-center items-center shadow-2xl rounded-lg border-x-rose-600 p-5'>
-                    <RiCodeSSlashLine className='w-10 h-10 block mx-auto mt-4 mb-6'></RiCodeSSlashLine>
+                    <RiCodeSSlashLine className='w-10 h-10 block mx-auto mt-4 mb-6 text-secondary'></RiCodeSSlashLine>
                     <div >
                         <h2 className='text-primary font-semibold lg:text-2xl text-xl text-center mb-4'>Web Development</h2>
                         <p className='text-center'>Providing quality content on your website, regularly adding new information, establishing trust, and marketing your site on other websites and social media.</p>
                     </div>
                 </div>
                 <div className=' max-w-sm mx-auto flex-col justify-center items-center shadow-2xl rounded-lg border-x-rose-600 p-5'>
-                    <BsCodeSquare className='w-10 h-10 block mx-auto mt-4 mb-6'></BsCodeSquare>
+                    <BsCodeSquare className='w-10 h-10 block mx-auto mt-4 mb-6 text-secondary'></BsCodeSquare>
                     <div >
                         <h2 className='text-primary font-semibold lg:text-2xl text-xl text-center mb-4'>Customize Website</h2>
                         <p className='text-center  p-2'>I will cutomize your previous website, fix bugs, errors, and add new features. Providing the best and quality services.</p>
