e61d40015b6737f228415b04da1eb361cec9ff7f
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-05-05 06:04:07 +0600

    skills updated

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -5,13 +5,15 @@ export type TSkills = {
 
 export const skills = {
   Web: [
-    { name: "React", percent: 90 },
-    { name: "Node JS", percent: 80 },
-    { name: "Express JS", percent: 90 },
+    { name: "React.js", percent: 90 },
+    { name: "Next.js", percent: 90 },
+    { name: "Node.js", percent: 80 },
+    { name: "Express.js", percent: 90 },
     { name: "MongoDB", percent: 90 },
     { name: "Redux", percent: 80 },
     { name: "TypeScript", percent: 85 },
     { name: "Mongoose", percent: 80 },
+    { name: "PostgreSQL", percent: 80 },
     { name: "Prisma", percent: 85 },
   ],
   Others: [
@@ -19,8 +21,10 @@ export const skills = {
     { name: "Divi", percent: 90 },
     { name: "Elementor", percent: 95 },
     { name: "NoSQL", percent: 90 },
-    { name: "PostgreSQL", percent: 85 },
     { name: "MySQL", percent: 80 },
+    { name: "RESTful APIs:", percent: 80 },
+    { name: "Responsive Web Design", percent: 80 },
+    { name: "E-commerce Platforms", percent: 75 },
   ],
   Programming: [
     { name: "JavaScript", percent: 90 },
@@ -30,7 +34,9 @@ export const skills = {
     { name: "Git", percent: 90 },
     { name: "Github", percent: 90 },
     { name: "VS Code", percent: 85 },
-    { name: "Figma", percent: 80 },
+    { name: "Postman", percent: 80 },
+    { name: "npm/Yarn", percent: 85 },
+    { name: "Figma", percent: 75 },
     { name: "Photoshop", percent: 60 },
     { name: "Chrome Dev Tool", percent: 75 },
   ],
